9756d90c8f1e9c4ac033ec5480864f58cddf2d61
unfa <unfa00@gmail.com>
2019-10-22 19:40:55 +0200

    Updated

--- VitalInstaller.sh
+++ VitalInstaller.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+
+if [[ ! -e "./VitalInstaller.zip" ]]; then
+    echo "There's no VitalInstaller.zip in the current directory! Exiting"
+    exit
+fi
+
+unzip VitalInstaller.zip
+
 rsync -av --delete ./VitalBinaries/Vital.lv2 /usr/lib/lv2/Vital.lv2
 rsync -av --delete ./VitalBinaries/VitalFX.lv2 /usr/lib/lv2/VitalFX.lv2
 rsync -av --delete ./VitalBinaries/content /usr/share/Vital
 rsync -av ./VitalBinaries/vital /usr/bin/vital
 
+rm -rf ./VitalBinaries
 
+echo
+echo All done!
