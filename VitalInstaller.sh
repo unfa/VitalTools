afc8bcdb4e4d8b68f129c6a848b8fb803721405c
unfa <unfa00@gmail.com>
2019-10-24 16:53:56 +0200

    Updated to fix bad paths when installing fresh

--- VitalInstaller.sh
+++ VitalInstaller.sh
@@ -8,9 +8,11 @@ fi
 
 unzip VitalInstaller.zip
 
-rsync -av --delete ./VitalBinaries/Vital.lv2 /usr/lib/lv2/Vital.lv2
-rsync -av --delete ./VitalBinaries/VitalFX.lv2 /usr/lib/lv2/VitalFX.lv2
-rsync -av --delete ./VitalBinaries/content /usr/share/Vital
+mkdir -p /usr/lib/lv2/Vital.lv2 /usr/lib/lv2/VitalFX.lv2 /usr/share/Vital
+
+rsync -av --delete ./VitalBinaries/Vital.lv2/ /usr/lib/lv2/Vital.lv2/
+rsync -av --delete ./VitalBinaries/VitalFX.lv2/ /usr/lib/lv2/VitalFX.lv2/
+rsync -av --delete ./VitalBinaries/content/ /usr/share/Vital/
 rsync -av ./VitalBinaries/vital /usr/bin/vital
 
 rm -rf ./VitalBinaries
